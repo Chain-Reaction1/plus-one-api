a912774b5b029e8ad07dfa7faa94ff5228ff71bf
kbini28 <kbini28@gmail.com>
2020-07-15 11:03:36 -0400

    Event Schema and routes
    Added event schema and crud routes for events
    
    mob programming:
    @dalanah
    @andrew
    @frederick

---
+++ app/models/event.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const eventSchema = new mongoose.Schema({
+  place: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  guests: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Event', eventSchema)
